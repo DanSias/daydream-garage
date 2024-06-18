0d61904ed6cf9f2ff6147b02143cc17108cfc25d
DanSias <dan.sias@gmail.com>
2024-06-18 12:43:25 -0400

    Streamline Dashboard
    Added brands and cars with links to the car search on the dashboard.

--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -1,20 +1,24 @@
 <script setup>
-import AppLayout from '@/Layouts/AppLayout.vue';
-import Welcome from '@/Components/Welcome.vue';
+import AppLayout from "@/Layouts/AppLayout.vue";
+import DashboardCard from "@/Components/DashboardCard.vue";
 </script>
 
 <template>
     <AppLayout title="Daydream Garage Dashboard">
         <template #header>
-            <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
-                Daydream Garage
+            <h2
+                class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight"
+            >
+                Build Your Ultimate Car Collection
             </h2>
         </template>
 
         <div class="py-12">
             <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-                <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-xl sm:rounded-lg">
-                    <Welcome />
+                <div
+                    class="bg-white dark:bg-gray-800 overflow-hidden shadow-xl sm:rounded-lg"
+                >
+                    <DashboardCard />
                 </div>
             </div>
         </div>
